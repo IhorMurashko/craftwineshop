56d18a0aaafc0d2e74a5126d21bcbff143cc5f0b
Ihor Murashko <i.murashko0911@gmail.com>
2023-11-28 18:27:46 +0100

    added security configuration using spring security with jwttoken

---
+++ CraftWineShop/src/main/java/com/craftWine/shop/security/MyUserServiceDetails.java
@@ -0,0 +1,25 @@
+package com.craftWine.shop.security;
+
+
+import com.craftWine.shop.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserServiceDetails implements UserDetailsService {
+
+
+    private final UserRepository userRepository;
+
+    public MyUserServiceDetails(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUserByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found !"));
+    }
+
+}
