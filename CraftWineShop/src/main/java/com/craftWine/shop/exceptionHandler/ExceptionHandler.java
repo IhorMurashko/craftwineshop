293609e26b6e638995eaecfd4630763854883797
Ihor Murashko <i.murashko0911@gmail.com>
2023-11-28 18:42:02 +0100

    ExceptionHandler for the project

---
+++ CraftWineShop/src/main/java/com/craftWine/shop/exceptionHandler/ExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.craftWine.shop.exceptionHandler;
+
+import com.craftWine.shop.exceptions.EmailProblem;
+import com.craftWine.shop.exceptions.InvalidConfirmationPasswordException;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ExceptionHandler {
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(InvalidConfirmationPasswordException.class)
+    public ResponseEntity<ResponseException> invalidConfirmationPasswordExceptionHandler(@NotNull InvalidConfirmationPasswordException exception) {
+        return new ResponseEntity<ResponseException>(new ResponseException(exception.getMessage()), HttpStatus.CONFLICT);
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(EmailProblem.class)
+    public ResponseEntity<ResponseException> invalidConfirmationPasswordExceptionHandler(@NotNull EmailProblem exception) {
+        return new ResponseEntity<ResponseException>(new ResponseException(exception.getMessage()), HttpStatus.CONFLICT);
+    }
+
+}
