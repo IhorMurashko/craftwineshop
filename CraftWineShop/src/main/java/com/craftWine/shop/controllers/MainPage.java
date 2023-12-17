1d690a19bbfceffe40262f8974bc7f49cf1941f2
Ihor Murashko <i.murashko0911@gmail.com>
2023-12-17 12:12:47 +0100

    added test controller for connect woth front end

--- CraftWineShop/src/main/java/com/craftWine/shop/controllers/MainPage.java
+++ CraftWineShop/src/main/java/com/craftWine/shop/controllers/MainPage.java
@@ -1,5 +1,6 @@
 package com.craftWine.shop.controllers;
 
+import com.craftWine.shop.dto.CraftWineTest;
 import com.craftWine.shop.dto.wineDTO.CraftWineDTOResponse;
 import com.craftWine.shop.models.CraftWine;
 import com.craftWine.shop.service.CraftWineService;
@@ -7,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +19,28 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
+//@CrossOrigin
 @RestController
 @RequestMapping("/main")
 public class MainPage {
 
     @Autowired
     private final CraftWineService craftWineService;
+
+
+    @GetMapping(value = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<CraftWineTest>> testController() {
+
+        CraftWineTest test1 = new CraftWineTest("wine time shop", "src/main/wine_images/wine-7467.jpg");
+        CraftWineTest test2 = new CraftWineTest("meeting at 20:30", "src/main/wine_images/wine-7468.jpg");
+        CraftWineTest test3 = new CraftWineTest("and it works!", "src/main/wine_images/wine-7468.jpg");
+
+        return ResponseEntity.ok(new ArrayList<>() {{
+            add(test1);
+            add(test2);
+        }});
+
+    }
 
     @GetMapping(value = "/craft_wines", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<List<CraftWineDTOResponse>>> listResponseEntity() {
@@ -58,7 +76,7 @@ public class MainPage {
                                 craftWine.getBottlesSoldCounter(), craftWine.getAddedDateTime(), craftWine.isBestSeller()))
                         .filter(CraftWineDTOResponse::isBestSeller)
                         .sorted(Comparator.comparing(CraftWineDTOResponse::getBottlesSoldCounter).reversed())
-                                .collect(Collectors.toList());
+                        .collect(Collectors.toList());
 
 
         return ResponseEntity.ok(new ArrayList<>() {{
