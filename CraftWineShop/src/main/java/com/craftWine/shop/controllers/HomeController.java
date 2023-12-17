d89a0e77a2a62489945804ddcaf8469ea18fc7df
Ihor Murashko <i.murashko0911@gmail.com>
2023-12-17 12:10:33 +0100

    When a user tries to authorize, a check will be made to see if the token is present and if it is valid.

--- CraftWineShop/src/main/java/com/craftWine/shop/controllers/HomeController.java
+++ CraftWineShop/src/main/java/com/craftWine/shop/controllers/HomeController.java
@@ -3,9 +3,11 @@ package com.craftWine.shop.controllers;
 import com.craftWine.shop.authentication.RegistrationService;
 import com.craftWine.shop.authentication.UserRegisterAndAuthenticationService;
 import com.craftWine.shop.dto.authUserDTO.CredentialsDTO;
-import com.craftWine.shop.dto.authUserDTO.RegisterDTO;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.craftWine.shop.security.TokenProperties;
+import com.craftWine.shop.security.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -14,22 +16,15 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @CrossOrigin
 @Validated
+@Slf4j
+@RequiredArgsConstructor
 public class HomeController {
 
     private final RegistrationService registrationService;
     private final UserRegisterAndAuthenticationService userService;
-//    private final TokenProvider tokenProvider;
+    private final TokenProvider tokenProvider;
+    private final TokenProperties tokenProperties;
 
-
-    @Autowired
-    public HomeController(RegistrationService registrationService,
-                          UserRegisterAndAuthenticationService userService
-//                          ,TokenProvider tokenProvider
-    ) {
-        this.registrationService = registrationService;
-        this.userService = userService;
-//        this.tokenProvider = tokenProvider;
-    }
 
     @GetMapping("/")
     public ResponseEntity<String> homeController() {
@@ -37,16 +32,36 @@ public class HomeController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> loginController(@RequestBody CredentialsDTO credentialsDTO) {
+    public ResponseEntity<String> loginController(@RequestBody CredentialsDTO credentialsDTO,
+                                                  @RequestHeader(name = "Authorization", required = false) String headerToken
+    ) {
         try {
+
+            if (headerToken != null &&
+                    headerToken.startsWith("Bearer ") &&
+                    tokenProvider.validateToken(headerToken.substring(7))) {
+
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("You have already logged in");
+            }
+
+
             String token = userService.authenticate(credentialsDTO);
 
-            return ResponseEntity.ok(token);
+            log.info("new token: {}", token);
+
+            HttpHeaders header = new HttpHeaders();
+            header.setBearerAuth(token);
+
+            return ResponseEntity.status(HttpStatus.OK)
+                    .headers(header).build();
+
         } catch (Exception runtimeException) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid credentials");
 
         }
     }
+
 
 
     @GetMapping(path = "/confirm")
