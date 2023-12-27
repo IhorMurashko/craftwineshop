ea8c10f383a3a0193abbdb41bf0b91d7837861d4
Ihor Murashko <i.murashko0911@gmail.com>
2023-12-27 23:03:51 +0100

    deleted jetBrains annotations

--- CraftWineShop/src/main/java/com/craftWine/shop/exceptionHandler/ProjectExceptionHandler.java
+++ CraftWineShop/src/main/java/com/craftWine/shop/exceptionHandler/ProjectExceptionHandler.java
@@ -3,7 +3,6 @@ package com.craftWine.shop.exceptionHandler;
 import com.craftWine.shop.exceptions.CraftWineNotFoundException;
 import com.craftWine.shop.exceptions.EmailProblemException;
 import com.craftWine.shop.exceptions.InvalidConfirmationPasswordException;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,18 +16,18 @@ import java.sql.SQLException;
 public class ProjectExceptionHandler {
 
     @ExceptionHandler(InvalidConfirmationPasswordException.class)
-    public ResponseEntity<ResponseException> invalidConfirmationPasswordExceptionHandler(@NotNull InvalidConfirmationPasswordException exception) {
+    public ResponseEntity<ResponseException> invalidConfirmationPasswordExceptionHandler(InvalidConfirmationPasswordException exception) {
         return new ResponseEntity<ResponseException>(new ResponseException(exception.getMessage()), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(EmailProblemException.class)
-    public ResponseEntity<ResponseException> invalidConfirmationPasswordExceptionHandler(@NotNull EmailProblemException exception) {
+    public ResponseEntity<ResponseException> invalidConfirmationPasswordExceptionHandler(EmailProblemException exception) {
         return new ResponseEntity<ResponseException>(new ResponseException(exception.getMessage()), HttpStatus.CONFLICT);
     }
 
 
     @ExceptionHandler(SQLException.class)
-    public ResponseEntity<ResponseException> duplicateUniqueConstraintExceptionHandler(@NotNull DataIntegrityViolationException exception) {
+    public ResponseEntity<ResponseException> duplicateUniqueConstraintExceptionHandler(DataIntegrityViolationException exception) {
         if (exception.getMessage().contains("duplicate key")) {
             return new ResponseEntity<ResponseException>(new ResponseException(exception.getMessage()), HttpStatus.CONFLICT);
         } else {
@@ -38,17 +37,17 @@ public class ProjectExceptionHandler {
 
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ResponseException> illegalArgumentExceptionHandler(@NotNull IllegalArgumentException exception) {
+    public ResponseEntity<ResponseException> illegalArgumentExceptionHandler( IllegalArgumentException exception) {
         return new ResponseEntity<ResponseException>(new ResponseException(exception.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(IOException.class)
-    public ResponseEntity<ResponseException> failIOExceptionHandler(@NotNull IOException exception) {
+    public ResponseEntity<ResponseException> failIOExceptionHandler( IOException exception) {
         return new ResponseEntity<ResponseException>(new ResponseException("Failed input-output: " + exception.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(CraftWineNotFoundException.class)
-    public ResponseEntity<ResponseException> couldNotFoundCraftWine(@NotNull CraftWineNotFoundException exception) {
+    public ResponseEntity<ResponseException> couldNotFoundCraftWine(CraftWineNotFoundException exception) {
         return new ResponseEntity<ResponseException>(new ResponseException( exception.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
