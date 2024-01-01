bc713207b41c5c4b4309596771664a2c343ec85b
Ihor Murashko <i.murashko0911@gmail.com>
2024-01-01 16:49:28 +0100

    changed name of picture from article to id

--- CraftWineShop/src/main/java/com/craftWine/shop/utils/ImagineHandler.java
+++ CraftWineShop/src/main/java/com/craftWine/shop/utils/ImagineHandler.java
@@ -12,18 +12,18 @@ public class ImagineHandler {
     /**
      * Saves an image file into the server directory and returns the path.
      * <p>
-     * This method takes a {@code MultipartFile} representing an image file and a unique identifier {@code wineArticle},
+     * This method takes a {@code MultipartFile} representing an image file and a unique identifier {@code wineId},
      * saves the image to a server directory, and returns the path where the image is saved.
      *
      * @param imageFile   The {@code MultipartFile} representing the image file to be saved.
-     * @param wineArticle A unique identifier associated with the wine article.
+     * @param wineId A unique identifier associated with the wine article.
      * @return The path where the image is saved.
      * @throws IOException If an I/O exception occurs during the file copying process.
      */
-    public static String saveImageIntoServerAndReturnPath(MultipartFile imageFile, String wineArticle) throws IOException {
+    public static String saveImageIntoServerAndReturnPath(MultipartFile imageFile, String wineId) throws IOException {
         // Define the destination path for saving the image
         Path destination = Paths.get("/home/developer/my folder/java_projects/craftWineShop/CraftWineShop/src/main/wine_images/"
-                + "wine-" + wineArticle + ".jpg");
+                + "wine-" + wineId + ".jpg");
 
         // Copy the image file to the specified destination path, replacing existing files
         Files.copy(imageFile.getInputStream(), destination, StandardCopyOption.REPLACE_EXISTING);
