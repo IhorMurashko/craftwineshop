e4b831700ebd8cc9e14b5e1f50305bb7a8f5ebea
Ihor Murashko <i.murashko0911@gmail.com>
2024-01-01 17:02:11 +0100

    added mapper

--- CraftWineShop/src/main/java/com/craftWine/shop/mapper/CraftWineMapper.java
+++ CraftWineShop/src/main/java/com/craftWine/shop/mapper/CraftWineMapper.java
@@ -1,5 +1,6 @@
 package com.craftWine.shop.mapper;
 
+import com.craftWine.shop.dto.wineDTO.CraftWineDTO;
 import com.craftWine.shop.dto.wineDTO.CraftWineDTOResponse;
 import com.craftWine.shop.enumTypes.SugarConsistency;
 import com.craftWine.shop.enumTypes.WineColor;
@@ -7,6 +8,8 @@ import com.craftWine.shop.models.CraftWine;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
+
+import java.util.Arrays;
 
 @Mapper(componentModel = "spring", uses = {ProducedCountryMapper.class, RegionMapper.class, WineCommentMapper.class})
 public interface CraftWineMapper {
@@ -27,4 +30,27 @@ public interface CraftWineMapper {
     @Mapping(source = "wineColor", target = "wineColor", qualifiedByName = "wineColorToString")
     @Mapping(source = "sugarConsistency", target = "sugarConsistency", qualifiedByName = "sugarConsistencyToString")
     CraftWineDTOResponse toDTOResponse(CraftWine craftWine);
+
+    @Named("getWineColorEnumFromString")
+    default WineColor getWineColorEnum(String wineColor) {
+
+        return Arrays.stream(WineColor.values())
+                .filter(wine -> wine.getName().equals(wineColor))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Could not find color of wine"));
+    }
+
+    @Named("getSugarConsistencyEnumFromString")
+    default SugarConsistency getSugarConsistencyEnum(String sugarConsistency) {
+        return Arrays.stream(SugarConsistency.values())
+                .filter(color -> color.getName().equals(sugarConsistency))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Could not find sugar consistency of wine"));
+    }
+
+
+    @Mapping(source = "wineColor", target = "wineColor", qualifiedByName = "getWineColorEnumFromString")
+    @Mapping(source = "sugarConsistency", target = "sugarConsistency", qualifiedByName = "getSugarConsistencyEnumFromString")
+    CraftWine toEntityCraftWine(CraftWineDTO craftWineDTO);
+
 }
--- CraftWineShop/src/main/java/com/craftWine/shop/service/CraftWineServiceImpl.java
+++ CraftWineShop/src/main/java/com/craftWine/shop/service/CraftWineServiceImpl.java
@@ -63,7 +63,6 @@ public class CraftWineServiceImpl implements CraftWineService {
 
         craftWineRepository.save(craftWine);
         return craftWine;
-
     }
 
 
